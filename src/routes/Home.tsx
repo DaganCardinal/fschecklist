2ee2c0f6f805b4a43357a0a0031a0b3b4b9fe421
Dagan Cardinal <dagancardinal@gmail.com>
2023-08-19 20:41:10 -0600

    Styling updates:
    - Fixed issue with dropdown menus appearing off-screen on mobile
    - Fixed issue with repeating background image on mobile

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -6,26 +6,26 @@ export const Home = () => {
     <>
       <div className="flex flex-col justify-center mx-auto text-center mt-12 font-body">
         <h1 className="text-3xl font-bold font-heading mb-2">FSChecklist</h1>
-        <p className="text-xl mb-8">
+        <p className="text-xl mb-8 mx-3">
           A simple checklist web app for flight simulators
         </p>
         <p className="text-xl">Select an aircraft to get started</p>
       </div>
-      <div className="flex flex-row flex-wrap justify-between w-1/2 mx-auto mt-8">
-        <Link to="/b737-checklist">
-          <Button className="my-4">Boeing 737 NG</Button>
+      <div className="flex flex-col md:flex-row flex-wrap md:justify-between w-1/2 mx-auto mt-8">
+        <Link to="/b737-checklist" className="mx-auto">
+          <Button className="my-4 min-w-[9rem]">Boeing 737 NG</Button>
         </Link>
-        <Link to="/c208-checklist">
-          <Button className="my-4">Cessna 208B</Button>
+        <Link to="/c208-checklist" className="mx-auto">
+          <Button className="my-4 min-w-[9rem]">Cessna 208B</Button>
         </Link>
-        <Link to="/a320-checklist">
-          <Button className="my-4">Airbus A320</Button>
+        <Link to="/a320-checklist" className="mx-auto">
+          <Button className="my-4 min-w-[9rem]">Airbus A320</Button>
         </Link>
-        <Link to="/c172-checklist">
-          <Button className="my-4">Cessna 172</Button>
+        <Link to="/c172-checklist" className="mx-auto">
+          <Button className="my-4 min-w-[9rem]">Cessna 172</Button>
         </Link>
-        <Link to="/crjx-checklist">
-          <Button className="my-4">CRJ</Button>
+        <Link to="/crjx-checklist" className="mx-auto">
+          <Button className="my-4 min-w-[9rem]">CRJ</Button>
         </Link>
       </div>
     </>
