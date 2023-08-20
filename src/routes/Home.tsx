2c4856df7e404abd4cbd5b87d73b1ab1b0549d65
Dagan Cardinal <dagancardinal@gmail.com>
2023-08-19 18:03:44 -0600

    fixed link issue on homepage

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -12,19 +12,19 @@ export const Home = () => {
         <p className="text-xl">Select an aircraft to get started</p>
       </div>
       <div className="flex flex-row flex-wrap justify-between w-1/2 mx-auto mt-8">
-        <Link to="/737">
+        <Link to="/b737-checklist">
           <Button className="my-4">Boeing 737 NG</Button>
         </Link>
-        <Link to="/208">
+        <Link to="/c208-checklist">
           <Button className="my-4">Cessna 208B</Button>
         </Link>
-        <Link to="/a320">
+        <Link to="/a320-checklist">
           <Button className="my-4">Airbus A320</Button>
         </Link>
-        <Link to="/c172">
+        <Link to="/c172-checklist">
           <Button className="my-4">Cessna 172</Button>
         </Link>
-        <Link to="/crjx">
+        <Link to="/crjx-checklist">
           <Button className="my-4">CRJ</Button>
         </Link>
       </div>
