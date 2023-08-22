b50872e510a51133c5d853124e67e065248cfeea
Dagan Cardinal <dagancardinal@gmail.com>
2023-08-22 15:32:24 -0600

    Added warning message to editor page

--- src/routes/Editor.tsx
+++ src/routes/Editor.tsx
@@ -11,6 +11,17 @@ export const Editor = () => {
 
   return (
     <>
+      <div className="flex flex-row justify-center text-center mt-6">
+        <div className="rounded-lg p-2 bg-red-600 dark:bg-red-500 flex flex-col justify-center text-center text-gray-100">
+          <h1 className="font-heading tracking-wide text-3xl font-semibold text-center mb-4">
+            WARNING!
+          </h1>
+          <p className="font-body text-lg">
+            This section is still under development, checklists are NOT SAVED at
+            this time
+          </p>
+        </div>
+      </div>
       <ChecklistEditor onSave={handleSave} />
     </>
   );
